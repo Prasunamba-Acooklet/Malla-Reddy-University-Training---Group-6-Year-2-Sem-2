e46b7cf1b29397c6e93d311a371f5427b3491042
Prasunamba M.K <meherprasunamba.k@gmail.com>
2025-01-04 10:00:29 +0530

    while loop - name and pwd

---
+++ Demo006.java
@@ -0,0 +1,11 @@
+package Core_Java;
+public class Demo006 {
+  public static void main(String[] args) {
+    String name = "Prasunamba";
+    String pwd = "Prasunamba@123";
+    while ((name == "Prasunamba")&&(pwd == "Prasunamba_123")){
+        System.out.println("Hello Prasunamba welcome to Malla Reddy University");
+        break;
+    }
+  }
+}
